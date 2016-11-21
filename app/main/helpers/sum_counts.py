bfa67c64644b61a14b822668f94e9a81044005b4
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2016-11-21 17:23:44 +0000

    framework statistics: make small improvements to inner data processing routines
    Some are simply readability improvements, some improve memory efficiency

--- app/main/helpers/sum_counts.py
+++ app/main/helpers/sum_counts.py
@@ -1,19 +1,21 @@
 from datetime import datetime
 
+from six import iteritems
+
 
 def format_snapshots(snapshots, category, groupings):
-    return [
+    return tuple(
         _label_and_count(
             snapshot['data'][category], groupings, snapshot['createdAt']
         )
         for snapshot in snapshots
-    ]
+    )
 
 
 def _label_and_count(stats, groupings, created_at):
     data = {
         label: _sum_counts(stats, filters)
-        for label, filters in groupings.items()
+        for label, filters in iteritems(groupings)
     }
     data['created_at'] = created_at
     return data
@@ -22,15 +24,12 @@ def _label_and_count(stats, groupings, created_at):
 def _sum_counts(stats, filter_by=None, sum_by='count'):
     return sum(
         statistic[sum_by] for statistic in stats
-        if not filter_by or all(
-            _find(statistic.get(key), value)
-            for key, value in filter_by.items()
+        if (not filter_by) or all(
+            _check_for_inclusion(statistic.get(key), value)
+            for key, value in iteritems(filter_by)
         )
     )
 
 
-def _find(statistic, filter_value):
-    if isinstance(filter_value, list):
-        return statistic in filter_value
-    else:
-        return statistic == filter_value
+def _check_for_inclusion(statistic, filter_value):
+    return (statistic in filter_value) if isinstance(filter_value, list) else (statistic == filter_value)
