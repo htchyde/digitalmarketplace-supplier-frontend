e97866b25102ebe783e6f8f336e1c3c8447845ca
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2017-02-07 15:17:29 +0000

    new flow for adding services: one question per page

--- app/main/views/services.py
+++ app/main/views/services.py
@@ -406,6 +406,7 @@ def edit_service_submission(framework_slug, lot_slug, service_id, section_id, qu
     framework, lot = get_framework_and_lot(data_api_client, framework_slug, lot_slug, allowed_statuses=['open'])
 
     force_return_to_summary = request.args.get('return_to_summary') or framework['framework'] == "dos"
+    next_question = None
 
     try:
         draft = data_api_client.get_draft_service(service_id)['services']
@@ -421,6 +422,7 @@ def edit_service_submission(framework_slug, lot_slug, service_id, section_id, qu
     content = content_loader.get_manifest(framework_slug, 'edit_submission').filter(draft)
     section = content.get_section(section_id)
     if section and (question_slug is not None):
+        next_question = section.get_question_by_slug(section.get_next_question_slug(question_slug))
         section = section.get_question_as_section(question_slug)
 
     if section is None or not section.editable:
@@ -455,14 +457,13 @@ def edit_service_submission(framework_slug, lot_slug, service_id, section_id, qu
                 errors = section.get_error_messages(e.message)
 
         if not errors:
-            next_section = content.get_next_editable_section_id(section_id)
-
-            if next_section and request.form.get('continue_to_next_section') and not force_return_to_summary:
+            if next_question and not force_return_to_summary:
                 return redirect(url_for(".edit_service_submission",
                                         framework_slug=framework['slug'],
                                         lot_slug=draft['lotSlug'],
                                         service_id=service_id,
-                                        section_id=next_section))
+                                        section_id=section_id,
+                                        question_slug=next_question.slug))
             else:
                 return redirect(url_for(".view_service_submission",
                                         framework_slug=framework['slug'],
@@ -482,7 +483,7 @@ def edit_service_submission(framework_slug, lot_slug, service_id, section_id, qu
         "services/edit_submission_section.html",
         section=section,
         framework=framework,
-        next_section_name=get_next_section_name(content, section.id),
+        next_question=next_question,
         service_data=service_data,
         service_id=service_id,
         force_return_to_summary=force_return_to_summary,
